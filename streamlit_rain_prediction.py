2c6cec320356023e315e0a3910f37e070c04a106
Giantdika <giantdikaf@gmail.com>
2025-05-04 19:29:42 +0700

    streamlit_rain_prediction.py

--- streamlit_rain_prediction.py
+++ streamlit_rain_prediction.py
@@ -15,15 +15,15 @@ st.title("Prediksi Hujan Berdasarkan Data Cuaca")
 
 st.markdown("Masukkan data cuaca berikut untuk memprediksi apakah akan terjadi hujan.")
 
-min_temp = st.number_input("Min Temp (°C)", value=10.0)
-max_temp = st.number_input("Max Temp (°C)", value=25.0)
-humidity_9am = st.number_input("Humidity 9am (%)", value=70.0)
-humidity_3pm = st.number_input("Humidity 3pm (%)", value=50.0)
+temp_min = st.number_input("temp_min (°C)", value=10.0)
+temp_max = st.number_input("temp_max (°C)", value=25.0)
+precipitation = st.number_input("precipitation (%)", value=70.0)
+precipitation = st.number_input("precipitation (%)", value=50.0)
 pressure_9am = st.number_input("Pressure 9am (hPa)", value=1012.0)
 pressure_3pm = st.number_input("Pressure 3pm (hPa)", value=1008.0)
 
 if st.button("Prediksi"):
-    sample = np.array([[min_temp, max_temp, humidity_9am, humidity_3pm, pressure_9am, pressure_3pm]])
+    sample = np.array([[temp_min, temp_max, precipitation, precipitation, pressure_9am, pressure_3pm]])
     sample_scaled = scaler.transform(sample)
     prediction = model.predict(sample_scaled)
     result = le.inverse_transform([int(prediction[0] > 0.5)])
